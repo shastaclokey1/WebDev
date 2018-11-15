2e2e626a517b1450fff18250050054c4560a7744
shastaclokey1 <shasta.clokey@vmuv.io>
2018-11-14 19:05:17 -0800

    learned how to specify multiple tag identifiers $("tag1tag2") with no space. learned the css identifyier tag:first-of-type to identify the first encountered element in the jQuery selection

--- Projects/Unit 16 jQuery/practicingjQuery.js
+++ Projects/Unit 16 jQuery/practicingjQuery.js
@@ -1,7 +1,8 @@
 $("div").css("background", "purple");
 
-$(".highlight").css("width", "200px");
+$("div.highlight").css("width", "200px");
 
 $("#third").css("border", "2px dashed orange");
 
-$("div")[0].style.color = "pink";
+$("div:first-of-type").css("color", "pink");
+//$("div")[0].style.color = "pink";
