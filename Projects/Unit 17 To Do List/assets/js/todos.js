4b87a266764958a976a62b8a04e13e6b66c67ca0
shastaclokey1 <shasta.clokey@vmuv.io>
2019-02-13 06:19:44 -0800

    finished the todo list project by adding dynamic item creation, color gradiants, and other css styling. JQuery made this really easy Started taking notes on the patatap project to get familiar with libs

--- Projects/Unit 17 To Do List/assets/js/todos.js
+++ Projects/Unit 17 To Do List/assets/js/todos.js
@@ -1,12 +1,31 @@
 // Check off specific Todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 // Clik on X to delet Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
+
+$("#toggleForm").click(function(){
+	$("input[type='text']").fadeToggle();
+});
+
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13 && $(this).val() != ""){
+		//grab new todo text from input
+		var todoText = $(this).val();
+		//clear text input
+		$(this).val("");
+		//create a new todo item
+		AddItemToTodoList(todoText);
+	}
+});
+
+function AddItemToTodoList(item){
+	$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + item + "</li>");
+}
